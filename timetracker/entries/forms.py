393ed163a1f1c59c1f8a25b11692f155a5fd9795
ari.dyball@gmail.com <aridyball@192-168-1-8.tpgi.com.au>
2015-08-11 12:32:43 +1000

    This seems to work. Hopefully will have some time to attempt the bonuses.

--- timetracker/entries/forms.py
+++ timetracker/entries/forms.py
@@ -1,10 +1,53 @@
 from django import forms
 
-from .models import Project
+
+from .models import Project, Client, Entry
+from django.db import models
+from django.utils import timezone
+
+
+
 
 
 class EntryForm(forms.Form):
     start = forms.DateTimeField(label="Start Time", help_text="Format: 2006-10-25 14:30")
     end = forms.DateTimeField(label="End Time", help_text="Format: 2006-10-25 14:30")
     project = forms.ModelChoiceField(queryset=Project.objects.all())
-    description = forms.CharField()
+    description = forms.CharField(max_length=100)
+
+    def clean(self):
+        cleaned_data = super(EntryForm, self).clean()
+        start = self.cleaned_data.get('start')
+        end = self.cleaned_data.get('end')
+        now = timezone.now()
+
+        if start >= now or start >= end:
+            raise forms.ValidationError("Start time must be in the past, end time must be after start time")
+        return cleaned_data
+
+#using these functions below would create a 'Nonetype() >= datetime()' error. The only way i could
+#get rid of that was to put them in one function which means that the error output might be confusing
+#for the user.
+    '''def clean_start(self):
+        start = self.cleaned_data.get('start')
+        end = self.cleaned_data.get('end')
+        now = timezone.now()
+        if start >= now:
+    	    raise forms.ValidationError("Start time must be in the past")
+        return start
+
+
+    def clean_end(self):
+        start = self.cleaned_data.get('start')
+        end = self.cleaned_data.get('end')
+        if start >= end:
+            raise forms.ValidationError("Start time must be before end time")
+        return end'''
+
+
+class ProjectForm(forms.Form):
+  	client = forms.ModelChoiceField(queryset=Client.objects.all())
+  	name = forms.CharField(max_length=100)
+
+class ClientForm(forms.Form):
+	name = forms.CharField(max_length=100)
--- timetracker/entries/views.py
+++ timetracker/entries/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 
-from .forms import EntryForm
+from .forms import EntryForm, ProjectForm, ClientForm
 from .models import Client, Entry, Project
 
 
 def clients(request):
+
+    if request.method == "POST":
+        client_form = ClientForm(request.POST)
+        if client_form.is_valid():
+            client = Client()
+            client.name = client_form.cleaned_data['name']
+            client.save()
+            #empty form
+            client_form = ClientForm()  
+    else:
+        client_form = ClientForm(request.POST)
+
+
     client_list = Client.objects.all()
     return render(request, 'clients.html', {
         'client_list': client_list,
+        'client_form' : client_form,
     })
 
 
@@ -30,18 +44,35 @@ def entries(request):
             entry.project = entry_form.cleaned_data['project']
             entry.description = entry_form.cleaned_data['description']
             entry.save()
+            #create empty form
+            entry_form = EntryForm()
     else:
-        entry_form = EntryForm(request.POST)
+        entry_form = EntryForm()
 
     entry_list = Entry.objects.all()
     return render(request, 'entries.html', {
         'entry_list': entry_list,
         'entry_form': entry_form,
+        #'start' : entry_form.cleaned_data['start'],
     })
 
 
 def projects(request):
+    if request.method == 'POST':
+        project_form = ProjectForm(request.POST)
+        if project_form.is_valid():
+            project = Project()
+            project.client = project_form.cleaned_data['client']
+            project.name = project_form.cleaned_data['name']
+            project.save()
+            #empty form
+            project_form = ProjectForm()
+    else:
+        project_form = ProjectForm(request.POST)
+
     project_list = Project.objects.all()
     return render(request, 'projects.html', {
         'project_list': project_list,
+        #this is important
+        'project_form' : project_form,
     })
